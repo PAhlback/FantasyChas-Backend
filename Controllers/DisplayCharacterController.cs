e893c07482ef24c105f23d4f2c10156ff9cbc3b7
PAhlback <pontus.ahlback93@gmail.com>
2024-04-23 10:25:57 +0200

    Add get correct user in DisplayCharacterController

--- Controllers/DisplayCharacterController.cs
+++ Controllers/DisplayCharacterController.cs
@@ -7,9 +7,10 @@ namespace FantasyChas_Backend.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    [Authorize()]
+    [Authorize]
     public class DisplayCharacterController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> _userManager;
 
         public Character char1 = new Character()
         {
@@ -30,19 +31,22 @@ namespace FantasyChas_Backend.Controllers
 
         private readonly ILogger<DisplayCharacterController> _logger;
         //private readonly IdentityUser _user;
-        public DisplayCharacterController(ILogger<DisplayCharacterController> logger)//IdentityUser user
+        public DisplayCharacterController(ILogger<DisplayCharacterController> logger, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
-            //_user = user;
+            // Added UserManager to get access to the correct user in the entire DisplayCharacterController.
+            _userManager = userManager;
         }
 
-        [HttpPost("/GetCharacter")]
-        public IActionResult GetAllCharacters(IdentityUser user)// ta in aktiv user och välj characters för den
+        [HttpGet("/GetCharacter")]
+        public async Task<IActionResult> GetAllCharacters()// ta in aktiv user och välj characters för den
         {
             List<Character> characterList = new List<Character>();
 
             try
             {
+                IdentityUser user = await GetCurrentUserAsync();
+
                 characterList.Add(char1);
                 Character char2 = new Character() { UserId = user.Id };
                 characterList.Add(char2);
@@ -54,6 +58,8 @@ namespace FantasyChas_Backend.Controllers
             }
 
         }
+
+        private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(User);
     }
 
     //[Route("[controller]")]
