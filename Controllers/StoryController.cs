6f3254442f4ec8690b8fb249dea02ad7b5c8fb01
Balos87 <145317796+Balos87@users.noreply.github.com>
2024-05-13 09:27:56 +0200

    Pushed up what I'v done so far.

---
+++ Controllers/StoryController.cs
@@ -0,0 +1,40 @@
+﻿using FantasyChas_Backend.Data;
+using FantasyChas_Backend.Models;
+using FantasyChas_Backend.Models.DTOs;
+using FantasyChas_Backend.Models.ViewModels;
+using FantasyChas_Backend.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// Hämta karaktärernas ID, från frontend ? Kolla upp hur detta sker. Går det att se någonstans i fetchen?
+// Att göra: Kolla Frontends API endpoint, FÖRSÖK! hämta karaktärens id, om inte, försök hänvisa från .. repo? Kolla upp.
+// 
+
+namespace FantasyChas_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class StoryController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<StoryController> _logger;
+        private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(User);
+
+        public StoryController(ILogger<StoryController> logger, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        [HttpGet("/GetSelectedStory")]
+        public async Task<IActionResult> GetSelectedStory()//repo for stories
+        {
+
+        }
+
+    }
+}
+
+
---
+++ Models/DTOs/StoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FantasyChas_Backend.Models.DTOs
+{
+    public class StoryDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+    }
+}
