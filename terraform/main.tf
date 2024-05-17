edfd071eb800c3ba0aa311ae17e19993eccf080f
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-17 23:21:09 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,8 +21,12 @@ resource "azurerm_app_service_plan" "asp" {
   name                = "asp-fantasychas"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
 
 resource "azurerm_app_service" "app" {
