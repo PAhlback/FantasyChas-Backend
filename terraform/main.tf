b1e0667a0d554c4c5f137fb18b9abd819297cbcf
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-17 22:17:07 +0200

    changed sku

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_service_plan" "asp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "app" {
