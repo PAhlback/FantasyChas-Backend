91e75a4db80a2a96a5f81b727982776a7731ae3f
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-17 23:22:48 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,9 +34,8 @@ resource "azurerm_app_service" "app" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
-  https_only          = true
-
-  site_config {
+  https_only            = true
+  site_config { 
     minimum_tls_version = "1.2"
   }
 }
