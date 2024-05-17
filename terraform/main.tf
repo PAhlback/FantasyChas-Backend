d11ea1952543691c1805cc20cd75cb6a5ecfe619
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-18 00:44:52 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,7 +75,7 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile {
     computer_name  = "vm-fantasychas"
     admin_username = "azureuser"
-    admin_password = "varförfunkarlösernordetinte127!"
+    admin_password = "Varförfunkarlösernordetinte127!"
   }
 
   os_profile_linux_config {
