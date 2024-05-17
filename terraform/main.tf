f4d1f84f1bdacee32987c015ef3fe416a7fe4dcb
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-17 23:54:18 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~> 3.0.2"
     }
   }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
