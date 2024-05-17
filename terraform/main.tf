a02696677dd4d4478b852f3e2baa47e1b3fba818
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-17 23:51:54 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-fantasychas"
   location = "West Europe"
