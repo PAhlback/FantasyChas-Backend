4c40ed6dd9947d45ab46cd92115c1022349d6fe7
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-17 23:45:21 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,24 +3,102 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "asp" {
-  name                = "asp-fantasychas"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-fantasychas"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "app" {
-  name                = "app-fantasychas"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.asp.location
-  service_plan_id     = azurerm_service_plan.asp.id
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-fantasychas"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-  site_config {
-    always_on = true  # For example, enable always-on
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-fantasychas"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
-  app_settings = {
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-fantasychas"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm-fantasychas"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "vm-fantasychas"
+    admin_username = "azureuser"
+    admin_password = "1234"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    environment = "staging"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      echo "#!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y docker.io
+      sudo systemctl start docker
+      sudo systemctl enable docker
+      sudo docker login ghcr.io -u ${var.ghcr_username} -p ${var.ghcr_token}
+      sudo docker pull ${var.image_name}
+      sudo docker run -d -p 80:80 ${var.image_name}" > /tmp/init.sh
+      chmod +x /tmp/init.sh
+      /tmp/init.sh
+EOF
+  }
+}
+
+variable "ghcr_username" {
+  description = "GitHub Container Registry username"
+  type        = string
+}
+
+variable "ghcr_token" {
+  description = "GitHub Container Registry token"
+  type        = string
+}
+
+variable "image_name" {
+  description = "Docker image name"
+  type        = string
+  default     = "ghcr.io/f-eighty7/fantasychas-backend:latest"
 }
