a715137c3ca7436a92069bd10ebfc2da1c5f4556
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-17 23:26:22 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,26 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.0"
-    }
-  }
-  required_version = ">= 0.14.9"
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "rg-fantasychas"
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = "asp-fantasychas"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = "app-fantasychas"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-  https_only            = true
-  site_config { 
-    minimum_tls_version = "1.2"
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    always_on = true  # For example, enable always-on
+  }
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
   }
 }
