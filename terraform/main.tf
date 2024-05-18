d29a4517a2077c25a876a21e3317d45c8fce09a4
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-18 21:34:33 +0200

    Added tfsate storage in Terrarform cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,8 +80,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
