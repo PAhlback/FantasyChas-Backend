fa24630b7f3f5665abd1f6dd5cd205f64bb5d62a
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-18 22:27:07 +0200

    added ssh rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,7 +70,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
