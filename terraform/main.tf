f761737217f1741f519bab227d32995185efab2f
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-18 22:10:11 +0200

    Added public ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "FantasyChas-Backend-primary"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
