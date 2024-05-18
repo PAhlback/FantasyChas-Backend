c0a560a264c9f92b1c5a0a20bcce3a2435989deb
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-18 22:26:11 +0200

    added ssh rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,6 +58,24 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "FantasyChas-Backend-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "nic2" {
   name                = "FantasyChas-Backend-nic2"
   resource_group_name = azurerm_resource_group.rg.name
