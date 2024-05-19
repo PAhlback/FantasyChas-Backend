f0a36c9090455a6d8bbd6775c9d0e2f6d311b1e8
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-19 23:03:18 +0200

    updated workflow name

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,6 +94,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
   admin_username      = "fantasyadmin"
+    disable_password_authentication = true  # Disable password authentication
+
+  admin_ssh_key {
+    username   = "fantasyadmin"
+    public_key = file("~/.ssh/masterchass.pub")
+  }
   network_interface_ids = [
     azurerm_network_interface.nic.id,
     azurerm_network_interface.nic2.id,
@@ -121,12 +127,6 @@ runcmd:
   - systemctl enable docker
   - docker pull ${var.backend_docker_image}
   - docker run -d -p 80:80 ${var.backend_docker_image}
-
-write_files:
-  - path: /home/fantasyadmin/.ssh/authorized_keys
-    content: |
-      ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCu2tpC7EGCHD34wxHloZiXhCvNZtQtXE67/id/6VzaAH6xqukx1u3A8Ki7heuNqq2rDhIK38BEhxDM7KHEd0BEO+6as/TttzVIVz8LCoDBx1awTSdjBlpgopkQTP2nNzfDZMzMtysU/8b6CDASfpFBLomyQacN28pbte0RRAB5k5MTWizjT7FWpz/eBYUAtlWmsp3Qhh/FBxj7V2lYukxb/oMBV62ECuxzi0OUaGIryRrLQh/AOWo7dKkRXlSGec72YIGrb05nWqjzfSVWRMgYXDmCMbihAcA/MQZtWm14hRQjWJpkqBzEKKF4hM9+Ml0+W6y/J6wcAZwHkz8Nhnvikn3ttsFJujPt7QN9jBt7suA/gw/wnfa3S8gj0Xo8Q7YyIbysGJ4ixsMzregaYl3+Fr9m4sFr4tmEjza3ENh7Oo+lw75SsD25v7bZ3WkZ0ux2QDJM35I/TkC0hqgVVWpqkB/wPr7zt18PM0aoaW269HYol98iBMvn1CVnRrSbGwjhDRDGlTcTyjFBmayLj4+ls1031+0MCq1AjpRMaR8eCBgouMpJPcB7n+fwGRo6T1LhgVLu08L7xOd5p5agdA92QVkyLcm1D38q+fdPDN7vOCbFllNDmvNQFFDXaEexPAUa22/Sf65OrVJ7W6g2qh5hKNEGyHXVZMCnDFKnHDomsQ== ahink@AK-ROGStrix
-    permissions: 0600
 EOF
   )
 }
