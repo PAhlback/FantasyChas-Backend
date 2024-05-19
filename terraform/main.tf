65adc754cd8c4f8c8a315a0451cc8a0519f434b5
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-19 22:05:12 +0200

    updated to make frontend cicd work with backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "FantasyChas-Backend-pip"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"  
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -121,13 +121,26 @@ packages:
 runcmd:
   - systemctl start docker
   - systemctl enable docker
-  - docker pull ${var.docker_image}
-  - docker run -d -p 80:80 ${var.docker_image}
+  - docker pull ${var.backend_docker_image}
+  - docker pull ${var.frontend_docker_image}
+  - docker run -d -p 80:80 ${var.backend_docker_image}
+  - docker run -d -p 3000:3000 ${var.frontend_docker_image}
+
+write_files:
+  - path: /home/fantasyadmin/.ssh/authorized_keys
+    content: |
+      ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCu2tpC7EGCHD34wxHloZiXhCvNZtQtXE67/id/6VzaAH6xqukx1u3A8Ki7heuNqq2rDhIK38BEhxDM7KHEd0BEO+6as/TttzVIVz8LCoDBx1awTSdjBlpgopkQTP2nNzfDZMzMtysU/8b6CDASfpFBLomyQacN28pbte0RRAB5k5MTWizjT7FWpz/eBYUAtlWmsp3Qhh/FBxj7V2lYukxb/oMBV62ECuxzi0OUaGIryRrLQh/AOWo7dKkRXlSGec72YIGrb05nWqjzfSVWRMgYXDmCMbihAcA/MQZtWm14hRQjWJpkqBzEKKF4hM9+Ml0+W6y/J6wcAZwHkz8Nhnvikn3ttsFJujPt7QN9jBt7suA/gw/wnfa3S8gj0Xo8Q7YyIbysGJ4ixsMzregaYl3+Fr9m4sFr4tmEjza3ENh7Oo+lw75SsD25v7bZ3WkZ0ux2QDJM35I/TkC0hqgVVWpqkB/wPr7zt18PM0aoaW269HYol98iBMvn1CVnRrSbGwjhDRDGlTcTyjFBmayLj4+ls1031+0MCq1AjpRMaR8eCBgouMpJPcB7n+fwGRo6T1LhgVLu08L7xOd5p5agdA92QVkyLcm1D38q+fdPDN7vOCbFllNDmvNQFFDXaEexPAUa22/Sf65OrVJ7W6g2qh5hKNEGyHXVZMCnDFKnHDomsQ== ahink@AK-ROGStrix
+    permissions: 0600
 EOF
   )
 }
 
 variable "docker_image" {
   description = "The Docker image to use"
+  type        = string
+}
+
+variable "frontend_docker_image" {
+  description = "The Docker image for the frontend service"
   type        = string
 }
