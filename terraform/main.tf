c0b3b35faa4c8e1ed2bd4675e121575a8d3587e2
Ahin Khan <ahin.khan1@gmail.com>
2024-05-23 09:36:05 +0200

    Added a sql server

--- terraform/main.tf
+++ terraform/main.tf
@@ -185,6 +185,7 @@ resource "azurerm_network_interface" "sql_nic" {
     name                          = "FantasyChas-SQL-primary"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.1.5"
     public_ip_address_id          = azurerm_public_ip.sql_pip.id
   }
 }
