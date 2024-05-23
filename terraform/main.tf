a8532633327ca2d2ee690e04ad3f66ec5fbd1516
Ahin Khan <ahin.khan1@gmail.com>
2024-05-23 09:13:20 +0200

    Added a sql server

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,10 @@ terraform {
   }
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "FantasyChass"
+    organization = "FantasyChas"
 
     workspaces {
-      name = "FantasyChass-Backend"
+      name = "FantasyChas-Backend"
     }
   }
 }
@@ -20,38 +20,38 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "FantasyChass-Backend"
+  name     = "FantasyChas-Backend"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "FantasyChass-Backend-vnet"
+  name                = "FantasyChas-Backend-vnet"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "FantasyChass-Backend-subnet"
+  name                 = "FantasyChas-Backend-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                = "FantasyChass-Backend-pip"
+  name                = "FantasyChas-Backend-pip"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"  
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "FantasyChass-Backend-nic"
+  name                = "FantasyChas-Backend-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "FantasyChass-Backend-primary"
+    name                          = "FantasyChas-Backend-primary"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
@@ -59,7 +59,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "FantasyChass-Backend-nsg"
+  name                = "FantasyChas-Backend-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -74,30 +74,66 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SQL"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "nic2" {
-  name                = "FantasyChass-Backend-nic2"
+  name                = "FantasyChas-Backend-nic2"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
   ip_configuration {
-    name                          = "FantasyChass-Backend-internal"
+    name                          = "FantasyChas-Backend-internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "FantasyChass-Backend-vm"
+  name                = "FantasyChas-Backend-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
-  admin_username      = "fantasychassadmin"
-    disable_password_authentication = true 
+  admin_username      = "fantasychasadmin"
+  disable_password_authentication = true
   admin_ssh_key {
-    username   = "fantasychassadmin"
-    public_key = file("~/.ssh/fantasychass.pub")
+    username   = "fantasychasadmin"
+    public_key = file("~/.ssh/fantasychas.pub")
   }
   network_interface_ids = [
     azurerm_network_interface.nic.id,
@@ -128,6 +164,68 @@ runcmd:
   - docker pull ghcr.io/f-eighty7/fantasychas-backend/app:latest
   - docker run -d -p 8080:80 ghcr.io/f-eighty7/chaschallenger/app:latest
   - docker run -d -p 8081:80 ghcr.io/f-eighty7/fantasychas-backend/app:latest
+EOF
+  )
+}
+
+resource "azurerm_public_ip" "sql_pip" {
+  name                = "FantasyChas-SQL-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "sql_nic" {
+  name                = "FantasyChas-SQL-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "FantasyChas-SQL-primary"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.sql_pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "sql_vm" {
+  name                = "FantasyChas-SQL-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "sqladmin"
+  disable_password_authentication = true
+  admin_ssh_key {
+    username   = "sqladmin"
+    public_key = file("~/.ssh/fantasychas-sql.pub")
+  }
+  network_interface_ids = [azurerm_network_interface.sql_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<-EOF
+#cloud-config
+package_upgrade: true
+runcmd:
+  - apt-get update
+  - apt-get install -y curl
+  - curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+  - add-apt-repository "$(curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list)"
+  - apt-get update
+  - apt-get install -y mssql-server
+  - /opt/mssql/bin/mssql-conf setup
+  - systemctl start mssql-server
+  - systemctl enable mssql-server
 EOF
   )
 }
