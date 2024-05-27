4939f6b7dff1e81ba8fdd4233a24055290abcd5c
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-27 20:12:11 +0200

    trying nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -194,7 +194,7 @@ resource "azurerm_network_interface" "sql_nic" {
 }
 
 resource "azurerm_windows_virtual_machine" "sql_vm" {
-  name                = "FantasyChas-SQL-vm"
+  name                = "FantasyChasSQLvm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
