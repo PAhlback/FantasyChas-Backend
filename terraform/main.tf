15e03faa9e776d6cd8caf40b02b334062d048ac4
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-28 12:09:14 +0200

    trying nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -186,7 +186,7 @@ resource "azurerm_mssql_database" "mssql-db" {
   server_id      = azurerm_mssql_server.mssql-server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
+  max_size_gb    = 1
   read_scale     = true
   sku_name       = "S0"
   zone_redundant = true
