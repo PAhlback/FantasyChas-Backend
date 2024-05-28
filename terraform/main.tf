7a9cdc68a796fd6675b02fe4cb1f935d361ae986
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-28 13:41:30 +0200

    trying nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -186,7 +186,6 @@ resource "azurerm_mssql_database" "mssql-db" {
   name           = "fantasychas-db"
   server_id      = azurerm_mssql_server.mssql-server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
   max_size_gb    = 32
   read_scale     = true
   sku_name       = "GP_S_Gen5_1"
