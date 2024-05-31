946779aaa42142652dc49395ff0e2a643c68e544
Ahin Khan <ahin.khan@chasacademy.se>
2024-05-31 17:54:50 +0200

    cleaned up cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -197,10 +197,32 @@ EOF
   )
 }
 
+resource "azurerm_mssql_server" "mssql-server" {
+  name                         = "fantasy-sqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = var.sql_password
+}
+resource "azurerm_mssql_database" "mssql-db" {
+  name           = "fantasychas-db"
+  server_id      = azurerm_mssql_server.mssql-server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb    = 1
+  read_scale     = false
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
 variable "connection_string" {
   description = "Connection string for the database"
 }
 
 variable "openai_key" {
   description = "API key for OpenAI"
+}
+
+variable "sql password" {
+  description = "password for database"
 }
